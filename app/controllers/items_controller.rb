62868ced0019eef58d5f405610d106c22d0dd69f
ayuin6ix <ayuhannda19@gmail.com>
2021-05-31 09:49:16 +0900

    単体テスト修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,12 @@
 class ItemsController < ApplicationController
-
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-   #@items = Item.order("created_at DESC")
+    # @items = Item.order("created_at DESC")
   end
 
   def new
     @item = Item.new
-    
   end
 
   def create
@@ -23,6 +21,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_cost_id, :shipping_day_id, :prefecture_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_cost_id, :shipping_day_id,
+                                 :prefecture_id, :price).merge(user_id: current_user.id)
   end
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,21 +1,18 @@
 FactoryBot.define do
   factory :item do
-      
-      
-      name                       {"ピアノ"}
-      category_id                   {3}
-      description                {"ああああああ"}
-      price                      {'500'}
-      status_id                     {3}
-      prefecture_id                 {3}
-      shipping_cost_id              {3}
-      shipping_day_id               {3}
-      
-      after(:build) do |item|
-        item.image.attach(io: File.open('public/apple-touch-icon.png'), filename: 'apple-touch-icon.png')
-      end
+    name { 'ピアノ' }
+    category_id { 3 }
+    description                { 'ああああああ' }
+    price                      { 500 }
+    status_id                     { 3 }
+    prefecture_id                 { 3 }
+    shipping_cost_id              { 3 }
+    shipping_day_id               { 3 }
 
-      association :user 
-    
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/apple-touch-icon.png'), filename: 'apple-touch-icon.png')
+    end
+
+    association :user
   end
 end
