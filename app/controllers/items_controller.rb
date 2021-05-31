cfa38b23f1d5f42fdf8f2cf9fd6f06aeea6d83e2
ayuin6ix <ayuhannda19@gmail.com>
2021-05-31 18:38:49 +0900

    編集機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, only: [:new, :create]
+  
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -22,10 +24,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+       redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_cost_id, :shipping_day_id,
                                  :prefecture_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_prototype
+    @item = Item.find(params[:id])
+  end
+
+  
 end
